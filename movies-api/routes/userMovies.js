8d01c2b0d0294b4aabdd0a22d764195462422a99
Andres Ruiz <aruiz@grupooctano.com.mx>
2021-04-01 08:42:49 -0600

    Protegiendo nuestras rutas con Passport.js

--- movies-api/routes/userMovies.js
+++ movies-api/routes/userMovies.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const UserMoviesService = require('../services/userMovies');
+const passport = require('passport')
 
 const validationHandler = require('../utils/middleware/validationHandler');
 
@@ -7,12 +8,15 @@ const { movieIdSchema } = require('../utils/schemas/movies');
 const { userIdSchema } = require('../utils/schemas/users');
 const  { createUserMovieSchema } = require('../utils/schemas/userMovies');
 
+//ESTRATEGIA DE JWT
+require('../utils/auth/strategies/jwt');
+
 function userMoviesApi(app){
   const router = express.Router()
   app.use('/api/user-movies', router)
   const userMoviesService = new UserMoviesService()
 
-  router.get('/', validationHandler({ userId: userIdSchema}, 'query'),
+  router.get('/', passport.authenticate('jwt', {session: false}), validationHandler({ userId: userIdSchema}, 'query'),
   async function (req, res, next) {
     const { userId } = req.query
     try {
@@ -26,7 +30,7 @@ function userMoviesApi(app){
     }
   })
 
-  router.post('/', validationHandler(createUserMovieSchema), async function (req, res, next){
+  router.post('/', passport.authenticate('jwt', {session: false}), validationHandler(createUserMovieSchema), async function (req, res, next){
     const { body: userMovie } = req
     try {
       const createUserMovieId = await userMoviesService.createUserMovie({userMovie})
@@ -39,7 +43,7 @@ function userMoviesApi(app){
     }
   })
 
-  router.delete('/:userMovieId', validationHandler({ userMovieId: movieIdSchema }, 'params'), 
+  router.delete('/:userMovieId', passport.authenticate('jwt', {session: false}), validationHandler({ userMovieId: movieIdSchema }, 'params'), 
   async function (req, res, next) {
     const { userMovieId} = req.params
     try {
