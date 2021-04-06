d12ad18e7c6f4844ba46955f2fb48cd75b4e3cd0
Andres Ruiz <aruiz@grupooctano.com.mx>
2021-04-05 21:04:20 -0500

    Implementando Sign Provider en nuestra API

--- ssr-server/utils/auth/strategies/oauth.js
+++ ssr-server/utils/auth/strategies/oauth.js
@@ -1,20 +1,21 @@
 const passport = require('passport');
 const axios = require('axios');
 const boom = require('@hapi/boom');
-const { Oauth2Strategy } = require('passport-oauth');
+const { OAuth2Strategy } = require('passport-oauth');
 
 const { config } = require('../../../config');
 
-const GOOGLE_AUTHORIZATION_URL = 'https://accounts.google.com/o/oauth2/v2/auth';
-const GOOGLE_TOKEN_URL = 'https://www.googleapis.com/oauth2/v4/token';
-const GOOGLE_URSERINFO_URL = 'https://www.googleapis.com/oauth2/v3/userinfo';
+const GOOGLE_AUTHORIZATION_URL = "https://accounts.google.com/o/oauth2/v2/auth";
+const GOOGLE_TOKEN_URL = "https://www.googleapis.com/oauth2/v4/token";
+const GOOGLE_USERINFO_URL = "https://www.googleapis.com/oauth2/v3/userinfo";
 
-const oAuthStrategy = new Oauth2Strategy({
-  authorizationURL: GOOGLE_AUTHORIZATION_URL,
-  tokenURL: GOOGLE_TOKEN_URL,
-  clientID: config.googleClientId,
-  clienteSecret: config.googleClientSecret,
-  callbackURL: "/auth/google-oauth/callback"
+const oAuth2Strategy = new OAuth2Strategy(
+  {
+    authorizationURL: GOOGLE_AUTHORIZATION_URL,
+    tokenURL: GOOGLE_TOKEN_URL,
+    clientID: config.googleClientId,
+    clientSecret: config.googleClientSecret,
+    callbackURL: "/auth/google-oauth/callback"
 }, async function(accessToken, refreshToken, profile, cb){
   const { data, status } = await axios({
     url: `${config.apiUrl}/api/auth/sign-provider`,
@@ -33,22 +34,25 @@ const oAuthStrategy = new Oauth2Strategy({
   return cb(null, data);
 });
 
-oAuth2Strategy.userProfile = function(accessToken, done){
-  this._oauth2.get(GOOGLE_URSERINFO_URL, accessToken, (err, body) => {
-    if(err){
+oAuth2Strategy.userProfile = function(accessToken, done) {
+  this._oauth2.get(GOOGLE_USERINFO_URL, accessToken, (err, body) => {
+    if (err) {
       return done(err);
     }
+
     try {
       const { sub, name, email } = JSON.parse(body);
+
       const profile = {
         id: sub,
         name,
         email
-      }
+      };
+
       done(null, profile);
     } catch (parseError) {
-      cb(parseError)
+      return done(parseError);
     }
-  })
-}
+  });
+};
 passport.use("google-oauth", oAuth2Strategy);
